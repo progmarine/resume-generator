3cc6d65034b061b166c6756a2b9641cf13f3373d
Owner <sadibekovdinmukhamed@gmail.com>
2022-09-25 10:52:04 +0600

    Delete unused files

--- generateresume/src/main/kotlin/cool/project/generateresume/enums/DocumentStatus.kt
+++
@@ -1,6 +0,0 @@
-package cool.project.generateresume.enums
-
-enum class DocumentStatus {
-    DOWNLOAD_DOCUMENT,
-    RETRY_INPUT_REQUEST;
-}
--- generateresume/src/main/kotlin/cool/project/generateresume/rest/CustomWebResponse.kt
+++
@@ -1,8 +0,0 @@
-package cool.project.generateresume.rest
-
-import cool.project.generateresume.enums.DocumentStatus
-
-data class CustomWebResponse(
-    val action: DocumentStatus? = null,
-    val status: Int
-)
